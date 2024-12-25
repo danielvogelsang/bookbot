defab12e88b35a0796770ad8de978c1ff1386feb
danielvogelsang <daniel@vogelsang-lr.de>
2024-12-25 20:58:43 +0100

    basic functuality with word counter

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        words = file_contents.split()
+        print(count_words(words))
+
+def count_words(book):
+    word_counter = 0
+    for character in book:
+        word_counter += 1
+    return word_counter
+    
 main()
